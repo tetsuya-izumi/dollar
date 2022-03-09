27e8387b30c81e7cf630e17eb4cfc574ce041f7d
ttyizm00 <38588229+tetsuya-izumi@users.noreply.github.com>
2022-03-09 23:50:10 +0900

    Add files via upload

--- dollar/main.js
+++ dollar/main.js
@@ -1,9 +1,14 @@
-let price1 = 100;
-let price2 = 1234;
-let price3 = 12345678912345;
+// 変換する数値(Number)
+let price1 = 1234;
+let price2 = 123456789;
 
+// 変換を実行
 jpy_price1 = new Intl.NumberFormat().format(price1);
-jpy_price1 = new Intl.NumberFormat().format(price2);
+jpy_price2 = new Intl.NumberFormat().format(price2);
+
+// コンソールに出力
+console.log(jpy_price1 + '円'); // 1,234円
+console.log(jpy_price2 + '円'); // 123,456,789円
 
 let jpy_price1 = price1.toLocaleString('ja-JP', {style:'currency', currency: 'JPY', minimumFractionDigits: 2});
 let jpy_price2 = price1.toLocaleString('ja-JP', {style:'currency', currency: 'JPY', minimumFractionDigits: 2});
